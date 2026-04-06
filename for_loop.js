4d607e856a4ef09cf6e94ce966e8306ee9d12216
Shaurya Singh <shauryasingh96317@gmail.com>
2026-04-06 21:52:44 +0530

    Add files via upload

---
+++ for_loop.js
@@ -0,0 +1,9 @@
+// Find Factorial of a Number
+let num = 5;
+let factorial = 1;
+
+for (let i = 1; i <= num; i++) {
+    factorial = factorial * i;
+}
+
+console.log("Factorial of " + num + " is: " + factorial);
